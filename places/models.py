ad9addceaccdabd97e0286ebc68f08fd818dbbd6
Sergey Kozyr <thelichking9@gmail.com>
2020-09-03 09:17:28 +0300

    Add Image model

--- places/models.py
+++ places/models.py
@@ -11,3 +11,12 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    index = models.SmallIntegerField()
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image_file = models.ImageField()
+
+    def __str__(self):
+        return f'{self.index} {self.place}'
