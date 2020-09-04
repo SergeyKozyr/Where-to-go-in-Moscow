0a1d13014ff95087aad891ab4709e663527b1ce8
Sergey Kozyr <thelichking9@gmail.com>
2020-09-04 08:25:06 +0300

    Add image thumbnail on place admin page

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['thumbnail', ]
+    fields = ('image_file', 'thumbnail', 'index')
+
+    def thumbnail(self, instance):
+        return format_html('<img src={} height={} />', instance.image_file.url, 200)
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
-    pass
