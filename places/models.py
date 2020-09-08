8980cb9f19b3645590db3678fe033d7297b230fa
Sergey Kozyr <thelichking9@gmail.com>
2020-09-08 10:50:34 +0300

    Update models' fields

--- places/models.py
+++ places/models.py
@@ -3,22 +3,28 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=100)
-    description_short = models.TextField()
-    description_long = HTMLField()
-    longitude = models.FloatField()
-    latitude = models.FloatField()
+    title = models.CharField('Название', max_length=100)
+    description_short = models.TextField('Короткое описание', blank=True)
+    description_long = HTMLField('Полное описание', blank=True)
+    longitude = models.FloatField('Долгота')
+    latitude = models.FloatField('Широта')
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    index = models.PositiveSmallIntegerField(default=0, blank=False, null=False)
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    image_file = models.ImageField()
+    index = models.PositiveSmallIntegerField('Позиция', default=0, blank=False, null=False)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Место на карте')
+    image_file = models.ImageField('Фотография')
 
     class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
         ordering = ['index']
 
     def __str__(self):
